abefdc505a18e95a6ca6ad75adaaa130a732b98b
shayan <shaian.heidari@gmail.com>
2025-05-06 15:52:58 +0330

    init

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -15,18 +15,15 @@ const Background = () => {
               </h3>
               <p className="text-pretty text-text text-[1.1rem] h-55 md:h-auto overflow-y-scroll">
                 <h4 className="italic text-[1rem] text-accent">2020 – 2024</h4>
-                Ph.D. in Computer Science,{" "}
-                <span className="font-bold">
-                  The University of Auckland
-                </span>{" "}
+                Ph.D. in Computer Science, The University of Auckland
                 <br />
                 Thesis:{" "}
                 <a href="#" className="italic underline hover:text-accent">
                   Quantum Annealing in Computer Vision
                 </a>
                 <h4 className="italic text-[1rem] text-accent">2014 – 2016</h4>
-                M.Sc. in Software Engineering, Science and Research Branch,{" "}
-                <span className="font-bold">Azad University</span>
+                M.Sc. in Software Engineering, Science and Research Branch, Azad
+                University
               </p>
             </div>
             <div className="card_ h-[20vh]">
@@ -35,12 +32,9 @@ const Background = () => {
               </h3>
               <p className="text-pretty text-text text-[1.1rem] h-45 md:h-auto overflow-y-scroll">
                 <h4 className="italic text-[1rem] text-accent">2024 – 2025</h4>
-                Postdoctoral Fellow in Computer Science,{" "}
-                <span className="font-bold">
-                  The University of Auckland{" "}
-                </span>{" "}
-                <br /> Research Focus: AI model improvement strategies for
-                biosecurity computer vision applications.
+                Postdoctoral Fellow in Computer Science, The University of
+                Auckland <br /> Research Focus: AI model improvement strategies
+                for biosecurity computer vision applications.
               </p>
             </div>
             <div className="card_ h-[33vh] md:col-span-2">
@@ -51,8 +45,8 @@ const Background = () => {
                 <h4 className="italic text-[1rem] text-accent">
                   2025 – Present
                 </h4>
-                Research Fellow, Institute of Marine Science,{" "}
-                <span className="font-bold">The University of Auckland</span>{" "}
+                Research Fellow, Institute of Marine Science, The University of
+                Auckland
                 <br /> Research Focus: Advancing computer vision,
                 photogrammetry, and underwater remote sensing techniques for the
                 monitoring and analysis of benthic ecosystems, including
